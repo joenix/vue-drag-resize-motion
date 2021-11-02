62b539f42a1320009b72a44360f7dd5bfd8cbc11
Joenix <joenix@qq.com>
2021-11-02 10:52:12 +0800

    feat: add span as flex

--- src/observer.vue
+++ src/observer.vue
@@ -1,20 +1,18 @@
 <style lang="less" scoped>
 .motion--observer {
   overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.motion--item {
+  flex: 1;
 }
 </style>
 
 <template>
   <div id="observer" class="motion--observer">
-    <div
-      class="motion--item"
-      v-for="(item, index) in source"
-      :key="index"
-      :draggable="!(item.meta || {}).disabled"
-      ref="observer"
-      @dragstart="e => observerDragstart(e, item, index)"
-      @dragend="e => observerDragend(e, item)"
-    >
+    <div class="motion--item" :style="{ flex: `0 0 ${100 / span}%` }" v-for="(item, index) in source" :key="index" :draggable="!(item.meta || {}).disabled" ref="observer" @dragstart="e => observerDragstart(e, item, index)" @dragend="e => observerDragend(e, item)">
       <slot name="item" :item="item" />
     </div>
   </div>
@@ -22,10 +20,10 @@
 
 <script>
 // Use Mixins
-import mixins from "./mixins.js";
+import mixins from './mixins.js';
 
 // Use Preset
-import { bus, defaultObserver } from "./preset";
+import { bus, defaultObserver } from './preset';
 
 // Component
 export default {
@@ -37,7 +35,7 @@ export default {
       type: [Array],
       default() {
         return [];
-      }
+      },
     },
 
     // Observer as Drag
@@ -45,8 +43,16 @@ export default {
       type: [Object],
       default() {
         return defaultObserver;
-      }
-    }
+      },
+    },
+
+    // Span as Flex
+    span: {
+      type: [Number],
+      default() {
+        return 1;
+      },
+    },
   },
 
   methods: {
@@ -59,7 +65,7 @@ export default {
 
       // Reset Observer
       this.reset();
-    }
-  }
+    },
+  },
 };
 </script>
